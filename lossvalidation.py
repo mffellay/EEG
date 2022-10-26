1765fae8801d31bedec651119f7086c46ae83b5e
MarcelFF <37382177+mffellay@users.noreply.github.com>
2022-10-26 20:53:00 +0200

    Update lossvalidation.py

--- lossvalidation.py
+++ lossvalidation.py
@@ -1,29 +1,28 @@
-# Aquí se graficarán las pérdidas de validación y entrenamiento ocurridas durante el proceso de creación o
-#ajuste del modelo del Autoencoder. Primero se tiene para 5 épocas. 
+# Model training and validation loss graph for 5, 15, 25 and 50 epochs 
 plt.plot(history.history["loss"], label="Pérdida entrenamiento") 
 plt.plot(history.history["val_loss"], label="Pérdida validación") 
 plt.title('Pérdidas en 5 épocas', fontsize=12) 
 plt.legend() 
-# Para 15 épocas. 
+# 15 epochs 
 plt.figure() 
 plt.plot(history2.history["loss"], label="Pérdida entrenamiento") 
 plt.plot(history2.history["val_loss"], label="Pérdida validación") 
 plt.title('Pérdidas en 15 épocas', fontsize=12) 
 plt.legend() 
-# Para 25 épocas. 
+# 25 epochs 
 plt.figure() 
 plt.plot(history3.history["loss"], label="Pérdida entrenamiento") 
 plt.plot(history3.history["val_loss"], label="Pérdida validación") 
 plt.title('Pérdidas en 25 épocas', fontsize=12) 
 plt.legend() 
-# Para 50 épocas. 
+# 50 epochs. 
 plt.figure() 
 plt.plot(history4.history["loss"], label="Pérdida entrenamiento") 
 plt.plot(history4.history["val_loss"], label="Pérdida validación") 
 plt.title('Pérdidas en 50 épocas', fontsize=12) 
 plt.legend() 
-# Este segmento de código corresponde al ajuste del modelo entrenado a 50 épocas al ingresar una entrada 
-# distinta al canal 1. Se utilizó el canal 2 de la señal EEG para poder visualizar el modelo siendo aplicado. 
+# Model fit test after 50 epochs and graph.
+# Channel 2 was used for visualization. 
 plt.figure() 
 encoded_data = autoencoder.encoder(atest_data).numpy() 
 decoded_data = autoencoder.decoder(encoded_data).numpy() 
